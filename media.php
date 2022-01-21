2adaca56bffd4ed066fdc8ccf29195c23be92402
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-01-21 16:33:14 -0300

    Aula -02

---
+++ media.php
@@ -0,0 +1,77 @@
+<?php
+
+    //Declaração de variáveis nomeVar = (tipoDeDados) valorInicial
+
+    $nota1 = (double) 0;
+    $nota2 = (double) 0;
+    $nota3 = (double) 0;
+    $nota4 = (double) 0;
+    $media = (double) 0;
+
+    //Recebendo os valores das caixas de texto utilizando o POST do formulário
+    $nota1 = $_POST['txtn1'];
+    $nota2 = $_POST['txtn2'];
+    $nota3 = $_POST['txtn3'];
+    $nota4 = $_POST['txtn4'];
+
+    //Calculo da Média
+    $media = ($nota1 + $nota2 + $nota3 + $nota4) /4 ;
+
+    echo($media)
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Média</title>
+       <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta charset="utf-8">
+    </head>
+	<body>
+        
+        <div id="conteudo">
+            <header id="titulo">
+                Calculo de Médias
+            </header>
+
+            <div id="form">
+                <form name="frmMedia" method="post" action="media.php">
+                    <div>
+                        <label>Nota 1:</label>
+                        <input type="text" name="txtn1" value=""  > 
+                    </div>
+                    
+                    <div>
+                        <label>Nota 2:</label>
+                        <input type="text" name="txtn2" value="" > 
+                    </div>
+                    
+                    <div>
+                        <label>Nota 3:</label>
+                        <input type="text" name="txtn3" value="" > 
+                    </div>
+                    
+                    <div>
+                        <label>Nota 4:</label>
+                        <input type="text" name="txtn4" value="" >
+                    </div>
+                    <div>
+                        <input type="submit" name="btncalc" value ="Calcular" >
+                        <div id="botaoReset">
+                            <a href="media.php">
+                                Novo Cálculo
+                            </a>    
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            <footer id="resultado">
+                A média é:
+            </footer>
+        </div>
+        
+		
+	</body>
+
+</html>
